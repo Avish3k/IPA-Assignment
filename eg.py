c4d70d4a97236bc774867f36d181f4f340608ca7
Avishek <avishek@gmail.com>
2026-02-20 19:48:35 +0530

    eg.py commited

---
+++ eg.py
@@ -0,0 +1,14 @@
+def evod(n):
+    num=n
+    e=0
+    o=0
+    while(num!=0):
+        d=num%10
+        if(d%2==0):
+            e+=1
+        elif(d%2!=0):
+            o+=1
+        num=num//10
+    print("no of even are",e)
+    print("no of odd are",o)
+evod(845236)
